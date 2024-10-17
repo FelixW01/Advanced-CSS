473112ecf4b465f3f027f48cafa269de2205d68a
Felix W <felixwillem01@yahoo.com>
2024-10-16 22:28:21 -0400

    fixed a bug where cards weren't being rendered properly

--- productGrid/productGrid.js
+++ productGrid/productGrid.js
@@ -12,22 +12,49 @@ const products = [
   image: "https://via.placeholder.com/150",
   alt: "an image of a cat in a bat costume",
   description: "Pet Cat Bat Wings for Halloween Party Decoration, cute Cat Dress Up Accessories"
+}, {
+  title: "Product 2",
+  price: "$6.99",
+  image: "https://via.placeholder.com/150",
+  alt: "an image of a cat in a bat costume",
+  description: "Pet Cat Bat Wings for Halloween Party Decoration, cute Cat Dress Up Accessories"
+}, {
+  title: "Product 2",
+  price: "$6.99",
+  image: "https://via.placeholder.com/150",
+  alt: "an image of a cat in a bat costume",
+  description: "Pet Cat Bat Wings for Halloween Party Decoration, cute Cat Dress Up Accessories"
+}, {
+  title: "Product 2",
+  price: "$6.99",
+  image: "https://via.placeholder.com/150",
+  alt: "an image of a cat in a bat costume",
+  description: "Pet Cat Bat Wings for Halloween Party Decoration, cute Cat Dress Up Accessories"
+}, {
+  title: "Product 2",
+  price: "$6.99",
+  image: "https://via.placeholder.com/150",
+  alt: "an image of a cat in a bat costume",
+  description: "Pet Cat Bat Wings for Halloween Party Decoration, cute Cat Dress Up Accessories"
+}, {
+  title: "Product 2",
+  price: "$6.99",
+  image: "https://via.placeholder.com/150",
+  alt: "an image of a cat in a bat costume",
+  description: "Pet Cat Bat Wings for Halloween Party Decoration, cute Cat Dress Up Accessories"
+}, {
+  title: "Product 2",
+  price: "$6.99",
+  image: "https://via.placeholder.com/150",
+  alt: "an image of a cat in a bat costume",
+  description: "Pet Cat Bat Wings for Halloween Party Decoration, cute Cat Dress Up Accessories"
 },
+
 ]
 
-function initializeElements() {
-  // create required elements
-  const cardDiv = document.createElement("div");
-  const cardH2 = document.createElement("h2");
-  const cardP = document.createElement("p");
-  const cardImg = document.createElement("img");
-}
-
 function populateProduct() {
-  initializeElements()
   for (let i = 0; i < products.length; i++) {
     let cardTemplate = `
-        <div class="div-container" id="product-div">
         <div class="div-card">
           <h2>${products[i].title}</h2>
           <p>$${products[i].price}</p>
@@ -37,7 +64,7 @@ function populateProduct() {
           </p>
         </div>`
 
-        productDiv.innerHTML = cardTemplate;
+        productDiv.innerHTML += cardTemplate;
       }
 }
 
